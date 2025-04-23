71b726c918f1296d83849687afc5215915db92d1
Maria Augusta Fialho <93688427+22augusta@users.noreply.github.com>
2025-04-23 10:46:40 -0300

    Create main.java

---
+++ minhasMusicas/main.java
@@ -0,0 +1,37 @@
+package minhasMusicas.principal;
+
+import minhasMusicas.modelos.MinhasPreferidas;
+import minhasMusicas.modelos.Musica;
+import minhasMusicas.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Olhos nos olhos");
+        minhaMusica.setCantor("Maria Bethânia");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("Podpeople");
+        meuPodcast.setApresentador("Ana Beatriz Barbosa");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+    }
+}
